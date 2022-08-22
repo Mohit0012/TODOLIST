8aacfca6b58916148f56f7f7dcf68c6895ee4b91
Mohit Sharma <99589514+Mohit0012@users.noreply.github.com>
2022-08-22 17:44:01 +0530

    Add files via upload

---
+++ To do list application/main.js
@@ -0,0 +1,22 @@
+ let addToDoButton = document.getElementById("addTODO");
+ let ToDoContainer =document.getElementById("ToDoContainer");
+ let inputField =document.getElementById("inputField");
+
+ addToDoButton.addEventListener('click',function()
+ {
+    var paragraph =document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    ToDoContainer.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener('click',function(){
+        paragraph.style.textDecoration = "line-through";
+
+    })
+    paragraph.addEventListener('dblclick',function(){
+    ToDoContainer.removeChild(paragraph);
+    })
+
+
+
+ })
